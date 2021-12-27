e2c72dbb700528f3408cbbce48c57b9d859ecd33
Susumu Yamazaki <zacky1972@gmail.com>
2021-12-27 13:25:41 +0900

    initial commit

---
+++ lib/nx_math.ex
@@ -0,0 +1,18 @@
+defmodule NxMath do
+  @moduledoc """
+  Documentation for `NxMath`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NxMath.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nx_math_test.exs
@@ -0,0 +1,8 @@
+defmodule NxMathTest do
+  use ExUnit.Case
+  doctest NxMath
+
+  test "greets the world" do
+    assert NxMath.hello() == :world
+  end
+end
