237cf85a3eb6dcf7e016e8f2a95058825522d839
Susumu Yamazaki <zacky1972@gmail.com>
2021-12-27 13:28:13 +0900

    set version to 0.1.0-dev

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NxMath.MixProject do
   def project do
     [
       app: :nx_math,
-      version: "0.1.0",
+      version: "0.1.0-dev",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
